ff93da0e3066a347601bb9291fcaf8a4d50f6a12
fernando gomes <fernando.gmsoli@gmail.com>
2025-03-10 15:35:48 -0300

    build: add dependencies

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,6 +1,8 @@
 plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.kotlin.android)
+    alias(libs.plugins.ksp)
+    alias(libs.plugins.dagger.hilt)
 }
 
 android {
@@ -18,7 +20,11 @@ android {
     }
 
     buildTypes {
+        debug {
+            buildConfigField("String", "BASE_URL", "\"https://api.mercadolibre.com/\"")
+        }
         release {
+            buildConfigField("String", "BASE_URL", "\"https://api.mercadolibre.com/\"")
             isMinifyEnabled = false
             proguardFiles(
                 getDefaultProguardFile("proguard-android-optimize.txt"),
@@ -33,15 +39,26 @@ android {
     kotlinOptions {
         jvmTarget = "11"
     }
+    configurations.implementation{
+        exclude(group = "com.intellij", module = "annotations")
+    }
+    buildFeatures {
+        buildConfig = true
+        viewBinding = true
+    }
 }
 
 dependencies {
-
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.appcompat)
     implementation(libs.material)
     implementation(libs.androidx.activity)
     implementation(libs.androidx.constraintlayout)
+    implementation(libs.androidx.legacy.support.v4)
+    implementation(libs.androidx.lifecycle.livedata.ktx)
+    implementation(libs.androidx.lifecycle.viewmodel.ktx)
+    implementation(libs.androidx.fragment.ktx)
+    //implementation(libs.androidx.runtime.android)
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
@@ -55,9 +72,11 @@ dependencies {
     implementation(libs.androidx.navigationCommon)
     implementation(libs.androidx.navigationFragment)
     implementation(libs.androidx.navigationUi)
-    implementation(libs.androidx.roomktx)
-    implementation(libs.androidx.roomcompiler)
+    ksp(libs.androidx.roomcompiler)
     implementation(libs.androidx.roomruntime)
     implementation(libs.kotlinx.coroutinesCore)
     implementation(libs.kotlinx.coroutinesAndroid)
+    implementation(libs.hilt.android)
+    ksp(libs.hilt.compiler)
+    implementation(libs.androidx.paging)
 }
